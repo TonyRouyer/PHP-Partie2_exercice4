fd77a206f1678d4f87e74696bb2a291420db3109
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 10:05:32 +0200

    correction

--- index.php
+++ index.php
@@ -9,26 +9,43 @@
     <p>
         <?php
             $magnitude = 9;
-            if ($magnitude == 1){
-                echo 'Micro-séisme impossible à ressentir.';
-            }else if ($magnitude == 2){
-                echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
-            }else if ($magnitude == 3){
-                echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
-            }else if ($magnitude == 4){
-                echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
-            }else if ($magnitude == 5){
-                echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
-            }else if ($magnitude == 6){
-                echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
-            }else if ($magnitude == 7){
-                echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
-            }else if ($magnitude == 8){
-                echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
-            }else if ($magnitude == 9){
-                echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
-            }
-
+            switch ($magnitude){
+                case 1: {
+                    echo 'Micro-séisme impossible à ressentir.';
+                    break;
+                }
+                case 2: {
+                    echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+                    break;
+                }
+                case 3: {
+                    echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
+                    break;
+                }
+                case 4: {
+                    echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+                    break;
+                }
+                case 5: {
+                    echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
+                    break;
+                }
+                case 6: {
+                    echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
+                    break;
+                }
+                case 7: {
+                    echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
+                    break;
+                }
+                case 8: {
+                    echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
+                break;
+                }
+                case 9: {
+                    echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+                break;
+                }
         ?>
     </p>
 </body>
