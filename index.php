a8b64644c28734977ae84c5a50b64c9608f0944f
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 16:01:01 +0200

    correction

--- index.php
+++ index.php
@@ -1,50 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>partie 2 exercice 4</title>
 </head>
 <body>
     <p>
         <?php
             $magnitude = 9;
-            switch ($magnitude){
-                case 1: {
+            switch ($magnitude) {
+                case 1:
                     echo 'Micro-séisme impossible à ressentir.';
                     break;
-                }
-                case 2: {
+                case 2:
                     echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
                     break;
-                }
-                case 3: {
+                case 3:
                     echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
                     break;
-                }
-                case 4: {
+                case 4:
                     echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
                     break;
-                }
-                case 5: {
+                case 5:
                     echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
                     break;
-                }
-                case 6: {
+                case 6:
                     echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
                     break;
-                }
-                case 7: {
+                case 7:
                     echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
                     break;
-                }
-                case 8: {
+                case 8:
                     echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
-                break;
-                }
-                case 9: {
-                    echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
-                break;
+                    break;
+                case 9:
+                    echo 'Séisme capable de tout détruire sur une très vaste zone.';
+                    break;
+                default:
+                    echo 'ce n\'est pas un nombre entre 1 et 9';
                 }
         ?>
     </p>
